1762de042568908920bed1bc5bb25bbdd1476f7d
Heena K <321910306025@gitam.in>
2021-08-17 17:38:46 +0530

    r

---
+++ Addition.py
@@ -0,0 +1,4 @@
+x = input("Enter the first number: ")
+y = input("Enter the second number: ")
+sum = int(x) + int(y)
+print("The sum is of the given numbers is: ", sum)
---
+++ Multiplication.py
@@ -0,0 +1,4 @@
+num_1 = input("Enter the first number: ")
+num_2 = input("Enter the second number: ")
+product = float(num_1) * float(num_2) 
+print("The product of the given numbers is:  ", product)
